b51aeb746f618ef50b23b900353e433edaa59ebc
Semyon <CEMEHEX@yandex.ru>
2018-12-22 17:18:46 +0300

    Fixed problems

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,8 +1,9 @@
-package controller;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+
+import java.awt.event.MouseEvent;
 
 public class Controller {
 
